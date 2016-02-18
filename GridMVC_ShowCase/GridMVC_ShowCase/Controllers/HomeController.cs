78cec216dfd83cf5bb84c185404bbca83e36db11
Sam.Z.D <samerzmd@gmail.com>
2016-02-19 01:15:06 +0200

    sample ready

--- GridMVC_ShowCase/GridMVC_ShowCase/Controllers/HomeController.cs
+++ GridMVC_ShowCase/GridMVC_ShowCase/Controllers/HomeController.cs
@@ -11,7 +11,18 @@ namespace GridMVC_ShowCase.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            List<Foo> list=new List<Foo>();
+            for (int i = 0; i < 100; i++)
+            {
+                Foo foo=new Foo {Title = "Title " + i,Description = "Description "+i };
+                list.Add(foo);
+            }
+            return View(list);
+        }
+        public class Foo
+        {
+            public string Title { get; set; }
+            public string Description { get; set; }
         }
     }
 }
